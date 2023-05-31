9ccac3fe6e62d9d150f23f9144a32063e2824216
Luke <lukemoore237@gmail.com>
2023-05-31 13:23:25 -0700

    Added more iNat data and created component.

---
+++ src/components/Observation.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let observation;
+    let uri = observation.uri
+</script>
+
+<div>
+    <h4>{observation.taxon.name}</h4>
+    <h5>{observation.taxon.preferred_common_name}</h5>
+    <p>Observed on {observation.observed_on}</p>
+</div>
+
+<style>
+    div {
+        border: 1px solid black;
+        border-radius: 1em;
+        
+    }
+</style>
+
